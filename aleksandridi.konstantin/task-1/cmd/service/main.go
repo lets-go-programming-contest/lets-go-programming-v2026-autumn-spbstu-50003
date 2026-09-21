1c1b87e31be5b6bd0cc14cae8b92b6f39d05c4b9
Kostas <kostas@MBA.local>
2026-09-20 21:39:42 -0700

    [TASK-1] Changed the name of the variables

--- aleksandridi.konstantin/task-1/cmd/service/main.go
+++ aleksandridi.konstantin/task-1/cmd/service/main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	num1, err := strconv.Atoi(scanner.Text())
+	firstOperand, err := strconv.Atoi(scanner.Text())
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 	scanner.Scan()
-	num2, err := strconv.Atoi(scanner.Text())
+	secondOperand, err := strconv.Atoi(scanner.Text())
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
@@ -25,17 +25,17 @@ func main() {
 	op := scanner.Text()
 	switch op {
 	case "+":
-		fmt.Println(num1 + num2)
+		fmt.Println(firstOperand + secondOperand)
 	case "-":
-		fmt.Println(num1 - num2)
+		fmt.Println(firstOperand - secondOperand)
 	case "*":
-		fmt.Println(num1 * num2)
+		fmt.Println(firstOperand * secondOperand)
 	case "/":
-		if num2 == 0 {
+		if secondOperand == 0 {
 			fmt.Println("Division by zero")
 			return
 		}
-		fmt.Println(num1 / num2)
+		fmt.Println(firstOperand / secondOperand)
 	default:
 		fmt.Println("Invalid operation")
 	}
